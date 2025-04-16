2746d89367d885d1086481992feb41f2b5ce2280
Pronoy Malick <malickpronoy@gmail.com>
2025-04-16 20:51:15 +0530

    created the initals in the pdf

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
 
 filepaths = glob.glob("invoices/*xlsx")
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
     print(df)
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    filename = Path(filepath).stem
+    invoice_no = filename.split("-")[0]
+    pdf.set_font(family="Helvetica", style="B", size=18)
+    pdf.cell(w=5, h=10,txt=f"Invoice No:- {invoice_no}")
+    pdf.output(f"PDF/{filename}.pdf")
